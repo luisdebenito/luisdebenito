761ccda63e659a8f3a8d2677752aa37d3f9a5104
Luis <luis.debenito2010@gmail.com>
2025-11-20 01:53:06 +0100

    yayy

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
-  styleUrls: ['./music.component.scss']
+  styleUrls: ['./music.component.scss'],
 })
-export class MusicComponent implements OnInit {
+export class MusicComponent {
+  videourls: SafeResourceUrl[] = [];
 
-  constructor() { }
+  public rawUrls: string[] = [
+    'https://www.youtube.com/embed/UdRM20NL0oY?si=1UdzgDE71K133uOl',
+    'https://www.youtube.com/embed/Z_HCiCQPM58?si=OvQk2nKjE3WTaWLK',
+    'https://www.youtube.com/embed/SbI0UjmGgSY?si=pFWHldjvxKMSLkwF',
+    'https://www.youtube.com/embed/KGH9TCKwkMw?si=LssKv5XUSq1xkFIE',
+    'https://www.youtube.com/embed/lhwLeB0mXjU?si=bNByZOGNmqzGm_Oy&amp;start=2178',
+  ];
 
-  ngOnInit(): void {
+  constructor(private sanitizer: DomSanitizer) {
+    this.videourls = this.rawUrls.map((url) =>
+      this.sanitizer.bypassSecurityTrustResourceUrl(url)
+    );
   }
-
 }
